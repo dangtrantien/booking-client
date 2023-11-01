fdf21484697aad27d3d72c7a548ff800e133f51f
Đặng Trần Tiến <tiendtfx22706@funix.edu.vn>
2023-11-01 11:52:32 +0700

    update server link

--- src/store/store.js
+++ src/store/store.js
@@ -13,6 +13,6 @@ const store = configureStore({
 });
 
 // export const host = 'http://192.168.1.107:5000';
-export const host = 'https://booking-server-6rik.onrender.com';
+export const host = 'https://booking-server-dangtrantien.vercel.app';
 
 export default store;
